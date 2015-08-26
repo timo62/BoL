196130199a6f7417e9daf0b3336b73ba44ccbe2a
HiranN <HiranN@users.noreply.github.com>
2015-08-26 18:08:40 -0300

    Update Teste.lua

--- Scripts/Teste.lua
+++ Scripts/Teste.lua
@@ -1,11 +1,21 @@
-local version = 0.001
+local version = 0.004
+local AUTOUPDATE = true
 
 	function Initiate()
-		printMessage = function(message) print("<font color=\"#00A300\"><b>"..scriptName..":</b></font> <font color=\"#FFFFFF\">"..message.."</font>") end --[[Replaces the standard print/PrintChat function to make the script look nicer]]
+	if AUTOUPDATE then
+			local scriptName = "Teste"
+		printMessage = function(message) print("<font color=\"#00A300\"><b>"..scriptName..":</b></font> <font color=\"#FFFFFF\">"..message.."</font>") end 
+		
+		--[[
 		if os.time() > os.time{year=2014, month=6, day=30, hour=0, sec=1} then
-			printMessage("A fail safe has disabled the script, contact the Author for access")
-			return true --[[Stops the script after a certain date, allows the author to update the script without people complaining about bugs]]
+			printMessage("Script disabled by : HiranN, contact him for access")
+			return true
 		end
-		SourceUpdater(scriptName, version, "raw.github.com", "/HiranN/BoL/Scripts/master/Scripts/Teste.lua", SCRIPT_PATH..GetCurrentEnv().FILE_NAME, "/HiranN/BoL/Scripts/master/Versions/Teste.version"):CheckUpdate()
+		--]]
+		
+		SourceUpdater(scriptName, version, "raw.github.com", "/HiranN/BoL/master/Scripts/Teste.lua", SCRIPT_PATH..GetCurrentEnv().FILE_NAME, "/HiranN/BoL/master/Versions/Teste.version"):CheckUpdate()
 		return false
 	end
+end
+
+	if Initiate() then return end
