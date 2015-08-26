2c2e4f87b3fc0eaaa155f864bf0b2f260c4b67da
HiranN <biel_ho@hotmail.com>
2015-08-26 17:15:39 -0300

    .

--- scripts/Teste.lua
+++ scripts/Teste.lua
@@ -1,13 +1,13 @@
-local version = "1.2"
+local version = "0.1"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/HiranN/BoL/master/scripts/SCRIPTNAME.lua".."?rand="..math.random(1,10000)
-local UPDATE_FILE_PATH = LIB_PATH.."SCRIPTNAME.lua"
+local UPDATE_PATH = "/HiranN/BoL/master/scripts/Teste.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = LIB_PATH.."Teste.lua"
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 function _AutoupdaterMsg(msg) print("<font color=\"#6698ff\"><b>SCRIPTNAME :</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/HiranN/BoL/master/versions/SCRIPTNAME.version")
+	local ServerData = GetWebResult(UPDATE_HOST, "/HiranN/BoL/master/versions/Teste.version")
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
