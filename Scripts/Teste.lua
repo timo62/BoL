18e3762abfdd5ecd3dab8821e622a14b9674bbbb
HiranN <HiranN@users.noreply.github.com>
2015-08-26 18:25:01 -0300

    Update Teste.lua

--- Scripts/Teste.lua
+++ Scripts/Teste.lua
@@ -1,16 +1,17 @@
-Version  = 0.1
-AutoUpdate  = true
-
-  function Update()
     local ToUpdate = {}
-    ToUpdate.UseHttps = true
-    ToUpdate.Host = "raw.githubusercontent.com"
-    ToUpdate.VersionPath = "/HiranN/BoL/master/Versions/Teste.version"
-    ToUpdate.ScriptPath =  "/HiranN/BoL/master/Scripts/Teste.lua"
-    ToUpdate.SavePath = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
-    ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) Msg("Updated from v"..OldVersion.." to "..NewVersion..". Please press F9 twice to reload.") end
-    ToUpdate.CallbackNoUpdate = function(OldVersion) end
-    ToUpdate.CallbackNewVersion = function(NewVersion) Msg("New version found v"..NewVersion..". Please wait until it's downloaded.") end
-    ToUpdate.CallbackError = function(NewVersion) Msg("There was an error while updating.") end
-    CScriptUpdate(Version,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
-  end
+    ToUpdate.Version = 0.1
+    DelayAction(function()
+        ToUpdate.UseHttps = true
+        ToUpdate.Host = "raw.githubusercontent.com"
+        ToUpdate.VersionPath = "/HiranN/BoL/master/Versions/Teste.version"
+        ToUpdate.ScriptPath =  "/HiranN/BoL/master/Scripts/Teste.lua"
+        ToUpdate.SavePath = SCRIPT_PATH.._ENV.FILE_NAME
+        ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) Print("Updated to v("..NewVersion..").  .Please reload script.") end
+        ToUpdate.CallbackNoUpdate = function(OldVersion) Print("No Updates Found.") end
+        ToUpdate.CallbackNewVersion = function(NewVersion) Print("New Version found ("..NewVersion.."). Please wait until its downloaded") end
+        ToUpdate.CallbackError = function(NewVersion) Print("Error while Downloading. Please try again.") end
+        SxScriptUpdate(ToUpdate.Version,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
+    end, 0.5)
+    Print("Version "..ToUpdate.Version.." loaded.")
+
+end
