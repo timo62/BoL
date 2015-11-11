b36e7c4f058aceff327e884af6dd9c50288a677a
HiranN <HiranN@users.noreply.github.com>
2015-11-11 02:35:55 -0200

    Update HR Draven Axes.lua

--- HR Draven Axes.lua
+++ HR Draven Axes.lua
@@ -1,16 +1,175 @@
 if myHero.charName ~= "Draven" then return end
 
+local ts
 local LocalVersion = "1.0"
 local autoupdate = true --Change to false if you don't wan't autoupdates
 local reticles = {}
-local wait
+local wait = nil
+local qBuff = 0
+local qStacks = 0
 
 function OnLoad()
   Menu = scriptConfig("HR Draven Axes", "Draven")
-	wait = nil
- 
-  orbwalkCheck()
-	findupdates()
+	
+	Menu:addSubMenu("Combo", "combo")
+	Menu.combo:addParam("UseQ", "Use Q", SCRIPT_PARAM_ONOFF, true)
+	Menu.combo:addParam("UseW", "Use W After Catch Axe", SCRIPT_PARAM_ONOFF, true)
+	Menu.combo:addParam("UseE", "Use E", SCRIPT_PARAM_ONOFF, true)
+	
+	Menu:addSubMenu("KillSteal", "killsteal")
+	Menu.killsteal:addParam("KSOn", "KillSteal", SCRIPT_PARAM_ONOFF, true)
+	Menu.killsteal:addParam("UseE", "Use E", SCRIPT_PARAM_ONOFF, true)
+	Menu.killsteal:addParam("UseR", "Use R", SCRIPT_PARAM_ONOFF, true)	
+	Menu.killsteal:addParam("RangeR", "Range R",  SCRIPT_PARAM_SLICE, 2500, 2000, 10000, 0) 
+	
+	Menu:addSubMenu("Keys", "keys")
+	Menu.keys:addParam("ComboKey", "Combo Key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+	Menu.keys:addParam("Harass", "Harass", SCRIPT_PARAM_ONKEYDOWN, false, 67)
+	Menu.keys:addParam("LaneClear", "LaneClear", SCRIPT_PARAM_ONKEYDOWN, false, 86)
+	
+	Menu:addSubMenu("Harass", "harass")
+	Menu.harass:addParam("UseQ", "Use Q", SCRIPT_PARAM_ONOFF, true)
+	
+	Menu:addSubMenu("LaneClear", "laneclear")
+	Menu.laneclear:addParam("UseQ", "Use Q", SCRIPT_PARAM_ONOFF, true)
+	
+	Menu:addSubMenu("Auto", "misc")
+	
+	Menu:addSubMenu("Draw Settings", "drawing")	
+	Menu.drawing:addParam("mDraw", "Disable All Range Draws", SCRIPT_PARAM_ONOFF, false)
+	Menu.drawing:addParam("myHero", "Draw My Range", SCRIPT_PARAM_ONOFF, true)
+	Menu.drawing:addParam("myColor", "Draw My Range Color", SCRIPT_PARAM_COLOR, {255, 100, 44, 255})
+	Menu.drawing:addParam("eDraw", "Draw Stand Aside (E) Range", SCRIPT_PARAM_ONOFF, true)
+	Menu.drawing:addParam("eColor", "Draw Stand Aside (E) Color", SCRIPT_PARAM_COLOR, {255, 100, 44, 255})
+	Menu.drawing:addParam("tColor", "Draw Target Color", SCRIPT_PARAM_COLOR, {255, 100, 44, 255})
+	Menu.drawing:addParam("tText", "Draw Current Target Text", SCRIPT_PARAM_ONOFF, true)
+	
+	CheckVPred()
+	Skills()		
+	IgniteCheck()
+	HealCheck()
+	BarrierCheck()
+	orbwalkCheck()
+	
+	enemyMinions = minionManager(MINION_ENEMY, myHero.range, myHero, MINION_SORT_HEALTH_ASC)
+	ts = TargetSelector(TARGET_LESS_CAST, 900, DAMAGE_PHYSICAL)
+	ts.name = "Draven"
+	Menu:addTS(ts)
+	PriorityOnLoad()
+end
+
+function PriorityOnLoad()
+	if heroManager.iCount < 10 or (TwistedTreeline and heroManager.iCount < 6) then
+		print("</b><font color=\"#FFFFFF\">HR Draven Axes : Too few champions to arrange priority.</font>")
+	elseif heroManager.iCount == 6 then
+		arrangePrioritysTT()
+    else
+		arrangePrioritys()
+	end
+end
+
+function OnDraw()
+	if not myHero.dead and not Menu.drawing.mDraw then
+    for i, reticle in ipairs(reticles) do
+      DrawCircle(reticle.x, reticle.y, reticle.z, 100, ARGB(255, 0, 0, 255))
+	end
+		if myHero:CanUseSpell(_E) == READY and Menu.drawing.eDraw then 
+			DrawCircle(myHero.x, myHero.y, myHero.z, 1050, RGB(Menu.drawing.eColor[2], Menu.drawing.eColor[3], Menu.drawing.eColor[4]))
+		end
+		
+		if Menu.drawing.myHero then
+			DrawCircle(myHero.x, myHero.y, myHero.z, myHero.range, RGB(Menu.drawing.myColor[2], Menu.drawing.myColor[3], Menu.drawing.myColor[4]))
+		end
+
+		if Target ~= nil and ValidTarget(Target) then
+			if Menu.drawing.tText then
+				DrawText3D("Current Target",Target.x-100, Target.y-50, Target.z, 20, 0xFFFFFF00)
+			end
+			DrawCircle(Target.x, Target.y, Target.z, 200, RGB(Menu.drawing.tColor[2], Menu.drawing.tColor[3], Menu.drawing.tColor[4]))
+		end
+	end
+end
+
+
+function CheckVPred()
+	if FileExist(LIB_PATH .. "/VPrediction.lua") then
+		require("VPrediction")
+		VP = VPrediction()
+	else
+		local ToUpdate = {}
+		ToUpdate.Version = 0.0
+		ToUpdate.UseHttps = true
+		ToUpdate.Name = "VPrediction"
+		ToUpdate.Host = "raw.githubusercontent.com"
+		ToUpdate.VersionPath = "/SidaBoL/Scripts/master/Common/VPrediction.version"
+		ToUpdate.ScriptPath =  "/SidaBoL/Scripts/master/Common/VPrediction.lua"
+		ToUpdate.SavePath = LIB_PATH.."/VPrediction.lua"
+		ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) print("<font color=\"#FF794C\"><b>" .. ToUpdate.Name .. ": </b></font> <font color=\"#FFDFBF\">Updated to "..NewVersion..". Please Reload with 2x F9</b></font>") end
+		ToUpdate.CallbackNoUpdate = function(OldVersion) print("<font color=\"#FF794C\"><b>" .. ToUpdate.Name .. ": </b></font> <font color=\"#FFDFBF\">No Updates Found</b></font>") end
+		ToUpdate.CallbackNewVersion = function(NewVersion) print("<font color=\"#FF794C\"><b>" .. ToUpdate.Name .. ": </b></font> <font color=\"#FFDFBF\">New Version found ("..NewVersion.."). Please wait until its downloaded</b></font>") end
+		ToUpdate.CallbackError = function(NewVersion) print("<font color=\"#FF794C\"><b>" .. ToUpdate.Name .. ": </b></font> <font color=\"#FFDFBF\">Error while Downloading. Please try again.</b></font>") end
+		ScriptUpdate(ToUpdate.Version,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
+	end
+end
+
+ function AutoIgnite()
+ if Ignite and ts.target ~= nil then
+ if Menu.killsteal.UseIgnite and ts.target.health <= 40 + (20 * myHero.level) and myHero:CanUseSpell(Ignite) == READY then
+  CastSpell(Ignite, ts.target)
+  end
+  end
+end
+
+ function AutoHeal()
+ if Heal then
+			if Menu.misc.AutoHeal and myHero:CanUseSpell(Heal) == READY then
+				if myHero.level > 5 and myHero.health/myHero.maxHealth < Menu.misc.HealCc/100 then
+					CastSpell(Heal)
+				elseif  myHero.level < 6 and myHero.health/myHero.maxHealth < (Menu.misc.HealCc/100)*.75 then
+					CastSpell(Heal)
+				end
+		end
+end
+end
+
+ function AutoBarrier()
+ if Barrier then
+			if Menu.misc.AutoBarrier and myHero:CanUseSpell(Barrier) == READY then
+				if myHero.level > 5 and myHero.health/myHero.maxHealth < Menu.misc.BarrierCc/100 then
+					CastSpell(Barrier)
+				elseif  myHero.level < 6 and myHero.health/myHero.maxHealth < (Menu.misc.BarrierCc/100)*.75 then
+					CastSpell(Barrier)
+				end
+		end
+end
+end
+
+ function HealCheck()
+if myHero:GetSpellData(SUMMONER_1).name:find("summonerheal") then Heal = SUMMONER_1
+  Menu.misc:addParam("AutoHeal", "Auto Heal", SCRIPT_PARAM_ONOFF, true)
+	Menu.misc:addParam("HealCc", "Heal",  SCRIPT_PARAM_SLICE, 15, 0, 100, 0) 
+elseif myHero:GetSpellData(SUMMONER_2).name:find("summonerheal") then Heal = SUMMONER_2
+  Menu.misc:addParam("AutoHeal", "Auto Heal", SCRIPT_PARAM_ONOFF, true)
+	Menu.misc:addParam("HealCc", "Heal",  SCRIPT_PARAM_SLICE, 15, 0, 100, 0) 
+ end
+end
+
+ function BarrierCheck()
+if myHero:GetSpellData(SUMMONER_1).name:find("summonerbar") then Barrier = SUMMONER_1
+  Menu.misc:addParam("AutoBarrier", "Auto Barrier", SCRIPT_PARAM_ONOFF, true)
+	Menu.misc:addParam("BarrierCc", "Barrier",  SCRIPT_PARAM_SLICE, 15, 0, 100, 0)
+elseif myHero:GetSpellData(SUMMONER_2).name:find("summonerbar") then Barrier = SUMMONER_2
+  Menu.misc:addParam("AutoBarrier", "Auto Barrier", SCRIPT_PARAM_ONOFF, true)
+	Menu.misc:addParam("BarrierCc", "Barrier",  SCRIPT_PARAM_SLICE, 15, 0, 100, 0)
+ end
+end
+
+function IgniteCheck()
+if myHero:GetSpellData(SUMMONER_1).name:find("summonerdot") then Ignite = SUMMONER_1
+  Menu.killsteal:addParam("UseIgnite", "Use Ignite", SCRIPT_PARAM_ONOFF, true)
+elseif myHero:GetSpellData(SUMMONER_2).name:find("summonerdot") then Ignite = SUMMONER_2
+  Menu.killsteal:addParam("UseIgnite", "Use Ignite", SCRIPT_PARAM_ONOFF, true)
+ end
 end
 
 function orbwalkCheck()
@@ -36,17 +195,13 @@ function orbwalkCheck()
 	end
 end
 
-function OnDraw()
-    for i, reticle in ipairs(reticles) do
-      DrawCircle(reticle.x, reticle.y, reticle.z, 100, ARGB(255, 0, 0, 255))
-  end
-end
-
 function OnCreateObj(obj)
     if obj ~= nil and obj.name ~= nil and obj.x ~= nil and obj.z ~= nil then
         if obj.name:find("reticle_self.troy") then
     table.insert(reticles, obj)
 		wait = obj
+        qStacks = qStacks + 1
+        qBuff = qBuff + 1
 		end
 		end
   end
@@ -57,15 +212,189 @@ function OnDeleteObj(obj)
     for i, reticle in ipairs(reticles) do
       if obj.x == reticle.x and obj.z == reticle.z then
         table.remove(reticles, i)
-        SxOrb:ForcePoint(nil)
+        qStacks = qStacks - 1
+        --SxOrb:ForcePoint(nil)
       end
     end
-	 end
+	 end     
+	 elseif obj.name == "Draven_Q_buf.troy" then
+        qBuff = qBuff - 1
   end
 end
 
-function OnTick()		
+function Skills()
+	SkillQ = { name = "Spinning Axe", range = nil, delay = nil, speed = nil, width = nil, ready = false }
+	SkillW = { name = "Blood Rush", range = nil, delay = nil, speed = nil, width = nil, ready = false }
+	SkillE = { name = "Stand Aside", range = 1050, delay = 0.25, speed = 1400, width = 160, ready = false }
+	SkillR = { name = "Whirling Death", range = math.huge, delay = 1.0, speed = 2000, width = 150, ready = false }
+
+	priorityTable = {
+			AP = {
+				"Annie", "Ahri", "Akali", "Anivia", "Annie", "Brand", "Cassiopeia", "Diana", "Evelynn", "FiddleSticks", "Fizz", "Gragas", "Heimerdinger", "Karthus",
+				"Kassadin", "Katarina", "Kayle", "Kennen", "Leblanc", "Lissandra", "Lux", "Malzahar", "Mordekaiser", "Morgana", "Nidalee", "Orianna",
+				"Ryze", "Sion", "Swain", "Syndra", "Teemo", "TwistedFate", "Veigar", "Viktor", "Vladimir", "Xerath", "Ziggs", "Zyra", "Velkoz", "Azir", "Ekko"
+			},
+			
+			Support = {
+				"Alistar", "Blitzcrank", "Janna", "Karma", "Leona", "Lulu", "Nami", "Nunu", "Sona", "Soraka", "Taric", "Thresh", "Zilean", "Braum"
+			},
+			
+			Tank = {
+				"Amumu", "Chogath", "DrMundo", "Galio", "Hecarim", "Malphite", "Maokai", "Nasus", "Rammus", "Sejuani", "Nautilus", "Shen", "Singed", "Skarner", "Volibear",
+				"Warwick", "Yorick", "Zac", "Tahm Kench", "Bard"
+			},
+			
+			AD_Carry = {
+				"Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Graves", "Jayce", "Jinx", "KogMaw", "Lucian", "MasterYi", "MissFortune", "Pantheon", "Quinn", "Shaco", "Sivir",
+				"Talon","Tryndamere", "Tristana", "Twitch", "Urgot", "Varus", "Vayne", "Yasuo", "Zed", "Kindred"
+			},
+			
+			Bruiser = {
+				"Aatrox", "Darius", "Elise", "Fiora", "Gangplank", "Garen", "Irelia", "JarvanIV", "Jax", "Khazix", "LeeSin", "Nocturne", "Olaf", "Poppy",
+				"Renekton", "Rengar", "Riven", "Rumble", "Shyvana", "Trundle", "Udyr", "Vi", "MonkeyKing", "XinZhao", "Gnar"
+			}
+	}
+
+end
+
+function SetPriority(table, hero, priority)
+	for i=1, #table, 1 do
+		if hero.charName:find(table[i]) ~= nil then
+			TS_SetHeroPriority(priority, hero.charName)
+		end
+	end
+end
+ 
+function arrangePrioritys()
+		for i, enemy in ipairs(GetEnemyHeroes()) do
+		SetPriority(priorityTable.AD_Carry, enemy, 1)
+		SetPriority(priorityTable.AP,	   enemy, 2)
+		SetPriority(priorityTable.Support,  enemy, 3)
+		SetPriority(priorityTable.Bruiser,  enemy, 4)
+		SetPriority(priorityTable.Tank,	 enemy, 5)
+		end
+end
+
+function arrangePrioritysTT()
+        for i, enemy in ipairs(GetEnemyHeroes()) do
+		SetPriority(priorityTable.AD_Carry, enemy, 1)
+		SetPriority(priorityTable.AP,       enemy, 1)
+		SetPriority(priorityTable.Support,  enemy, 2)
+		SetPriority(priorityTable.Bruiser,  enemy, 2)
+		SetPriority(priorityTable.Tank,     enemy, 3)
+        end
+end
+
+function OnTick()
+	if myHero.dead then return end
+	ts:update()
+	Target = GetCustomTarget()
+
+	ComboKey = Menu.keys.ComboKey
+	HarassKey = Menu.keys.Harass
+	LaneClearKey = Menu.keys.LaneClear
+	
+	if ComboKey then 
+	Combo(Target)
+	end
+	
+	if HarassKey and not ComboKey then
+	Harass(Target)
+	end	
+	
+	if LaneClearKey and not ComboKey then
+	LaneClear()
+	end
+	
+	if Menu.killsteal.KSOn then
+	KillSteal()
+	AutoIgnite()
+	end 
+	
+	if Menu.misc.AutoHeal then
+	AutoHeal()
+	end 
+	
+	if Menu.misc.AutoBarrier then
+	AutoBarrier()
+	end
 CatchAxes()
+end
+
+function GetCustomTarget()
+	ts:update()	
+	if ts.target and not ts.target.dead and ts.target.type == myHero.type then
+		return ts.target
+	else
+		return nil
+	end
+end
+
+function Combo(unit)
+	if ValidTarget(unit) and unit ~= nil and unit.type == myHero.type then
+	
+		if Menu.combo.UseQ and qStacks <= 0 then 
+			CastSpell(_Q)
+		end	
+		if Menu.combo.UseW and qBuff >= 1 then 
+			CastSpell(_W)
+		end	
+		if Menu.combo.UseE then 
+			CastE(unit)
+		end	
+	end
+end
+
+function Harass(unit)
+	if(myHero:CanUseSpell(_Q) == READY  and ts.target~=nil and Menu.harass.UseQ and qStacks <= 0 ) then 
+  CastSpell(_Q)
+	end
+end
+
+function LaneClear()
+	enemyMinions:update()
+		for i, minion in pairs(enemyMinions.objects) do
+			if ValidTarget(minion) and minion ~= nil then
+				if Menu.laneclear.UseQ and GetDistance(minion) <= myHero.range and myHero:CanUseSpell(_Q) == READY and qStacks <= 0 then
+					CastSpell(_Q, minion.x, minion.z)
+				end
+			end		 
+		end
+	end
+
+function KillSteal()
+	for _, unit in pairs(GetEnemyHeroes()) do
+		local health = unit.health
+		local dmgE = getDmg("E", unit, myHero)
+		local dmgR = getDmg("R", unit, myHero)
+		
+			if health < dmgE and Menu.killsteal.UseE and myHero:CanUseSpell(_E) == READY and ValidTarget(unit) then
+				CastE(unit)
+			if health < dmgR and Menu.killsteal.UseR and myHero:CanUseSpell(_R) == READY and ValidTarget(unit) and GetDistance(unit) <= Menu.killsteal.RangeR then
+				CastR(unit)
+			end
+	 end
+end
+end
+
+function CastE(unit)
+	if unit ~= nil and myHero:CanUseSpell(_E) == READY then
+		CastPosition,  HitChance,  Position = VP:GetLineAOECastPosition(unit, SkillE.delay, SkillE.width, SkillE.range, SkillE.speed, myHero)
+				
+		if HitChance >= 2 then
+			CastSpell(_E, CastPosition.x, CastPosition.z)
+		end
+	end
+end
+
+function CastR(unit)
+	if unit ~= nil and myHero:CanUseSpell(_R) == READY then
+		CastPosition,  HitChance,  Position = VP:GetLineAOECastPosition(unit, SkillR.delay, SkillR.width, SkillR.range, SkillR.speed, myHero)
+				
+		if HitChance >= 2 then
+			CastSpell(_R, CastPosition.x, CastPosition.z)
+		end
+	end
 end
 
 function CatchAxes()
