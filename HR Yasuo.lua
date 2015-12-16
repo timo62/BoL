790c8836ac39ddb5222e7e8b10436fcc13dc378c
HiranN <HiranN@users.noreply.github.com>
2015-12-16 01:04:32 -0200

    Update HR Yasuo.lua

--- HR Yasuo.lua
+++ HR Yasuo.lua
@@ -125,7 +125,7 @@ end
 	
 	enemyMinions = minionManager(MINION_ENEMY, 700, myHero, MINION_SORT_HEALTH_ASC)
 	jungleMinions = minionManager(MINION_JUNGLE, 700, myHero, MINION_SORT_MAXHEALTH_DEC)
-	ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, 1000, DAMAGE_MAGIC)
+	ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, 1000, DAMAGE_PHYSICAL)
 	ts.name = "Yasuo"
 	Menu:addTS(ts)
 	PriorityOnLoad()
