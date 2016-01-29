2c81f91a0c36dd79b40cbd4b20dd3f37db8f33ad
HiranN <HiranN@users.noreply.github.com>
2016-01-29 13:55:23 -0200

    Update HR AutoLvl.lua

--- HR AutoLvl.lua
+++ HR AutoLvl.lua
@@ -30,9 +30,10 @@ end
   }
 
 function AutoLvl:Tick()
-  if ListBlock[myHero.charName] then 
-  if myHero.level == 1 and myHero:GetSpellData(_Q).level <= 0 and myHero:GetSpellData(_W).level <= 0 and myHero:GetSpellData(_E).level <= 0 and os.clock() - self.LastSpell >= 1.0 then
-  self.LastSpell = os.clock()  
+  if ListBlock[myHero.charName] then
+  local Block = true 
+  if myHero.level == 1 and myHero:GetSpellData(_Q).level <= 0 and myHero:GetSpellData(_W).level <= 0 and myHero:GetSpellData(_E).level <= 0 and Block then
+  Block = false
   DelayAction(function()LevelSpell(Sequence[myHero.level]) end,0.5)
   end
   return 
@@ -165,7 +166,7 @@ end
 	elseif myHero.charName == 'Cassiopeia' then
   Sequence = {3,1,2,3,3,4,1,1,3,3,4,1,1,2,2,4,2,2}
 	elseif myHero.charName == 'Orianna' then
-  Sequence = {3,1,2,1,1,4,2,1,3,1,4,2,3,2,2,4,3,3}
+  Sequence = {1,2,3,1,1,4,2,1,3,1,4,2,3,2,2,4,3,3}
 	elseif myHero.charName == 'Lux' then
   Sequence = {3,1,2,3,3,4,1,2,3,1,4,2,3,1,1,4,2,2}
 	elseif myHero.charName == 'Diana' then
